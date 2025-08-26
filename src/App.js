75e1d988ea16ea8a78c3f2a761590a8eab986387
praneeshsuren <praneeshsuren@gmail.com>
2025-08-26 18:54:57 +0530

    fix: Hash Router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { HashRouter } from 'react-router-dom';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
@@ -13,19 +13,21 @@ import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <div className="bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 font-sans leading-relaxed transition-colors duration-500">
-      <Header />
-      <HeroSection />
-      <main className="container mx-auto px-6">
-        <AboutSection />
-        <SkillsSection />
-        <ExperienceSection />
-        <ProjectsSection />
-        <EducationSection />
-        <CertificationsSection />
-        <ContactSection />
-      </main>
-      <Footer />
-    </div>
+    <HashRouter>
+      <div className="bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 font-sans leading-relaxed transition-colors duration-500">
+        <Header />
+        <HeroSection />
+        <main className="container mx-auto px-6">
+          <AboutSection />
+          <SkillsSection />
+          <ExperienceSection />
+          <ProjectsSection />
+          <EducationSection />
+          <CertificationsSection />
+          <ContactSection />
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
